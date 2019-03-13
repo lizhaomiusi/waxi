280c62f1639b59bb77b5b7bb6a7ecf22c4fdb09d
muyoi <441505423@qq.com>
2019-03-13 22:33:12 +0800

    update

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,2 @@
 # My Documents
 https://waxi-i.github.io/waxi/
-
-欢迎使用ShowDoc！
-撒旦
-![](https://waxi-i.github.io/waxi/server/../Public/Uploads/2019-03-12/5c872735c99b1.png)
---
+++ sed.sh
@@ -0,0 +1,11 @@
+欢迎使用ShowDoc！
+撒旦
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-12/5c872735c99b1.png)
+
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-12/5c872735c99b1.png)
+
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-13/5c89062a13712.jpg)
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-13/5c890b9be3417.jpg)
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-13/5c890ba21c0a4.jpg)
+
+![](https://raw.githubusercontent.com/waxi-i/waxi/master/Uploads/2019-03-13/5c89140c44e4b.jpg)
